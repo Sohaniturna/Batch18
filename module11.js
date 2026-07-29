03fd8d16c693d90898d089f9cc89339dd27588c5
Sohani Fatehin turna <sohaniturna7@gmail.com>
2026-07-30 01:03:28 +0600

    First commit

---
+++ module11.js
@@ -0,0 +1,20 @@
+console.log("hi everyone");
+let name = "Turna";
+
+console.log("Hello, " + name);
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are not an adult");
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(10, 20);
+
+console.log(result);
